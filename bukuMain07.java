e3178a064d1a04f08e15bf91cd73d9dc119cb535
azkiyaihtiarilah <azkiyaihtiarilah03@gmail.com>
2024-02-20 13:56:28 +0700

    menambahkan book2

--- bukuMain07.java
+++ bukuMain07.java
@@ -2,16 +2,20 @@ package jobsheet2;
 
 public class bukuMain07 {
     public static void main(String[] args) {
-        buku07 book = new buku07();
-        book.judul = "Tentang Laut";
-        book.pengarang = "Azkiya Putri";
-        book.halaman = 100;
-        book.stock = 5;
-        book.harga = 10000;
+        buku07 book1 = new buku07();
+        book1.judul = "Tentang Laut";
+        book1.pengarang = "Azkiya Putri";
+        book1.halaman = 100;
+        book1.stock = 5;
+        book1.harga = 10000;
 
-        book.tampilInformasi();;
-        book.terjual(5);
-        book.gantiHarga(60000);
-        book.tampilInformasi();
+        book1.tampilInformasi();;
+        book1.terjual(5);
+        book1.gantiHarga(60000);
+        book1.tampilInformasi();
+
+        buku07 book2 = new buku07("Self Reward", "Ihtiarilah Azkiya", 160, 29, 59000);
+        book2.terjual(11);
+        book2.tampilInformasi();
     }
 }
